a26a1173983c7a8c411d6933d14358ad99c307d1
nicrausaz <n.crausaz99@gmail.com>
2019-01-18 12:14:14 +0100

    login form

--- www/src/components/definitions/LoginForm.js
+++ www/src/components/definitions/LoginForm.js
@@ -4,19 +4,35 @@ import { Button, Checkbox, Form } from 'semantic-ui-react'
 export default class LoginForm extends Component {
   state = {}
 
-  handleChange = (e, { value }) => this.setState({ value })
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = e => this.setState({[e.target.name]: e.target.value})
+
+  validate = () => { }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.validate()
+  }
+
 
   render() {
     return (
       <Form>
+      {JSON.stringify(this.state)}
         <Form.Field>
-          <Form.Input label='Email' type='email' placeholder='example@mail.com' onChange={this.handleChange} />
+          <Form.Input label='Email' type='email' placeholder='example@mail.com' name='email' value={this.state.email} onChange={this.handleChange} />
         </Form.Field>
         <Form.Field>
-          <Form.Input label='Mot de passe' type='password' placeholder='************' onChange={this.handleChange} />
-        </Form.Field>
-        <Form.Field>
-          <Checkbox label='Se souvenir de mon email' />
+          <Form.Input label='Mot de passe' type='password' placeholder='************' name="password" value={this.state.password} onChange={this.handleChange} />
         </Form.Field>
         <Button type='submit'>Submit</Button>
       </Form>
